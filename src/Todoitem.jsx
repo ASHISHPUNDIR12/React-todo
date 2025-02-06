43b97b5dda4af4832fe1039250f69f1b02e20116
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-02-06 19:23:11 +0530

    Refactor TodoItem, add delete functionality

---
+++ src/Todoitem.jsx
@@ -0,0 +1,40 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
+
+
+export default function Todoitem({ todo, removeTodo }) {
+    {
+        const labelId = `checkbox-list-label-${todo.list}`;
+
+        return (
+            <ListItem
+                key={todo.id}
+                secondaryAction={
+                    <IconButton edge="end" aria-label="comments">
+                        <CommentIcon onClick={removeTodo} />
+                    </IconButton>
+                }
+                disablePadding
+            >
+                <ListItemButton role={undefined} dense>
+                    <ListItemIcon  >
+                        <Checkbox
+                            edge="start"
+                            checked={todo.completed}
+                            tabIndex={-1}
+                            disableRipple
+                            inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                    </ListItemIcon >
+                    <ListItemText id={labelId} primary={todo.text} />
+                </ListItemButton>
+            </ListItem>
+        );
+    }
+
+}
